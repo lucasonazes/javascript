18806d8e4f9f3c304fe818c37fd9e965c5f51d08
lucasonazes <lucas.f@lafort.com.br>
2023-10-13 08:37:03 -0300

    Requests

--- Exercises/Requests/Axios (JSON)/JS/axios.js
+++ Exercises/Requests/Axios (JSON)/JS/axios.js
@@ -1,7 +1,33 @@
+//Fetch API
 fetch('pessoas.json')
     .then(response => response.json())
-    .then(json => loadElements(json))
+    .then(json => loadElements(json));
+
+//Axios
+axios('pessoas.json')
+    .then(response => loadElements(response.data));
+
 
 function loadElements(json) {
-    
+    const table = document.createElement('table');
+    for(let person of json) {
+        const tr = document.createElement('tr');
+
+        let td = document.createElement('td');
+        td.innerHTML = person.nome;
+        tr.appendChild(td);
+        
+        td = document.createElement('td');
+        td.innerHTML = person.idade;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerHTML = person.salario;
+        tr.appendChild(td);
+
+        table.appendChild(tr);
+    }
+
+    const result = document.querySelector('.result');
+    result.appendChild(table);
 }
