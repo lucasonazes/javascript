394087928b621b32f9d65848f5b2bc2fab19d3ad
lucasonazes <lucasonazes@gmail.com>
2023-11-28 14:30:31 -0300

    react tasklist completed

---
+++ REACT/tasklist/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parser: 'babel-eslint',
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'react/state-in-constructor': 0,
+    'jsx-a11y/control-has-associated-label': 0,
+    'react/forbid-prop-types': 0,
+    'import/no-extraneous-dependencies': 0
+  },
+};
