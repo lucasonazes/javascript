0e586211efcc3f49b76eb1b6acf9e41be041aeb0
lucasonazes <lucasonazes@gmail.com>
2024-01-24 17:27:00 -0300

    React on Standby, starting Typescript

--- API_REST/dist/controllers/HomeController.js
+++
@@ -1,7 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});class HomeController {
-  async index(req, res) {
-    res.json('API is running');
-  }
-}
-
-exports. default = new HomeController();
--- API_REST/src/controllers/HomeController.js
+++
@@ -1,7 +0,0 @@
-class HomeController {
-  async index(req, res) {
-    res.json('API is running');
-  }
-}
-
-export default new HomeController();
